06bfa677be44c6d554f3c31a8c309a817eaeb746
David Rodriguez <36946181+rofer0324@users.noreply.github.com>
2022-05-26 01:20:21 -0500

    Create Ejercicio1.py

---
+++ Ejercicios Introduccion/Ejercicio1.py
@@ -0,0 +1,11 @@
+"""
+Ejercicio 1:
+1) Rellenar la cadena del abecedario con el símbolo # hasta completar 30 caracteres.
+2) Mirar la longitud de la cadena abecedario.
+3) Poner en mayúsculas y en minúsculas la cadena.
+4) Buscar en la cadena abecedario la letra R y saber su posicion
+5) Crea otra cadena con las 5 letras que van después de la n
+6) Reemplazar la letra Z por Hola.
+7) Obtener una lista de la cadena abecedario y separarla por la letra i
+8) Unir la lista anterior con el símbolo “-” usando ‘-’.join(lista).
+"""
