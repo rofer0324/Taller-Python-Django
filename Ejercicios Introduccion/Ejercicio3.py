557c1778c51e4a7d5ee374e5523159a52d2e7108
David Rodriguez <36946181+rofer0324@users.noreply.github.com>
2022-05-31 20:01:16 -0500

    Create Ejercicio3.py
    Por comenzar el ejercicio 3.

---
+++ Ejercicios Introduccion/Ejercicio3.py
@@ -0,0 +1,8 @@
+"""
+Ejercicio 3:
+1) Crear una tupla con los meses del año
+2) Selecciona el mes noveno, y el penúltimo
+3) Selecciona todos los meses después de Agosto (no incluido)
+"""
+
+lista = ["Enero","Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Nomviembre", "Diciembre"]
