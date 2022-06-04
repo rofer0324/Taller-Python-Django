4b41df047c4c0a66d85199aecff592dab7453ad0
David Rodriguez <36946181+rofer0324@users.noreply.github.com>
2022-06-04 15:50:09 -0500

    Pequeños Cambios en los espacios.

--- Ejercicios Introduccion/Ejercicio3.py
+++ Ejercicios Introduccion/Ejercicio3.py
@@ -5,16 +5,11 @@ Ejercicio 3:
 3) Selecciona todos los meses después de Agosto (no incluido)
 """
 
-from traceback import print_list
-from typing import Tuple
-
-
 print("1) Crear una tupla con los meses del año")
 tupla = ("Enero","Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Nomviembre", "Diciembre")
 print(type(tupla))
 print(isinstance(tupla, tuple))  #Es tupla? => True
 #print(isinstance(tupla, list))  #Es lista? => False
-
 
 print("2) Selecciona el mes noveno, y el penúltimo")
 print(tupla[9] + ", " + tupla[11])
