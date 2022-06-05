1b3889c497d3053810b1306c41b31a961920a962
David Rodriguez <36946181+rofer0324@users.noreply.github.com>
2022-06-04 22:05:53 -0500

    Cambios de salto de linea.

--- Ejercicios Introduccion/Ejercicio3.py
+++ Ejercicios Introduccion/Ejercicio3.py
@@ -8,11 +8,11 @@ Ejercicio 3:
 print("1) Crear una tupla con los meses del año")
 tupla = ("Enero","Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Nomviembre", "Diciembre")
 print(type(tupla))
-print(isinstance(tupla, tuple))  #Es tupla? => True
+print(isinstance(tupla, tuple), "\n")  #Es tupla? => True
 #print(isinstance(tupla, list))  #Es lista? => False
 
 print("2) Selecciona el mes noveno, y el penúltimo")
-print(tupla[9] + ", " + tupla[11])
+print(tupla[9] + ", " + tupla[11], "\n")
 
 print("3) Selecciona todos los meses después de Agosto (no incluido)")
-print(tupla[8:12]) #Slicing
+print(tupla[8:12], "\n") #Slicing
